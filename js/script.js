3e820a97b1355860b588ae4c6b7f8b2b9ff2bd3a
TomasCastano <tomascastanochica@gmail.com>
2024-10-20 14:08:10 -0500

    Create the edition and creation of Posts with the PUT and DELETE methods of the JSONPlaceholder tool

--- js/script.js
+++ js/script.js
@@ -6,11 +6,14 @@ getPosts = () => {
             let posts = ''
             json.forEach(post => {
                 posts += `
-                <tr>
+                <tr id="post${post.id}">
                     <td>${post.id}</td>
                     <td>${post.title}</td>
                     <td>${post.body}</td>
                     <td>${post.userId}</td>
+                    <td class="actions-btns">
+                        <button id="delete${post.id}" class="btn btn-danger" onclick="deletePost(${post.id})">Eliminar</button>
+                    </td>
                 </tr>`
             })
             
@@ -22,6 +25,7 @@ getPosts = () => {
                     <th>Título</th>
                     <th>Descripción</th>
                     <th>Usuario</th>
+                    <th>Acciones</th>
                 </tr>
             </thead>
             <tbody>
@@ -69,3 +73,80 @@ const createUser = (event) => {
 
     console.log(title, body, userId)
 }
+
+const editPost = (event) => {
+    event.preventDefault()
+
+    const id = document.getElementById('postIdEdit').value  // ID ingresado por el usuario
+    const titleEdit = document.getElementById('titleEdit').value
+    const bodyEdit = document.getElementById('bodyEdit').value
+    const userIdEdit = document.getElementById('userIdEdit').value
+
+    if (!id) {
+        Swal.fire({
+            title: "Error!",
+            text: "Por favor, ingresa un ID válido",
+            icon: "error"
+        })
+        return
+    }
+
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+        method: 'PUT',
+        body: JSON.stringify({
+            title: title,
+            body: body,
+            userId: userId,
+        }),
+        headers: {
+            'Content-type': 'application/json charset=UTF-8',
+        },
+    })
+    .then((response) => response.json())
+    .then((json) => {
+        console.log(json)
+
+    })
+    .catch(error => console.log(error))
+   
+    document.getElementById('editCardTitle').textContent = `Título: ${titleEdit}`
+    document.getElementById('editCardBody').textContent = `Descripción: ${bodyEdit}`
+    document.getElementById('editCardUserId').textContent = `Usuario: ${userIdEdit}`
+
+    Swal.fire({
+        title: "Éxito!",
+        text: "Post editado correctamente",
+        icon: "success"
+    })
+}
+
+const deletePost = (id) => {
+    fetch(`https://jsonplaceholder.typicode.com/posts/${id}`, {
+        method: 'DELETE',
+    })
+    .then((response) => response.json())
+    .then((json) => console.log(json))
+    .catch(error => console.log(error))
+
+    Swal.fire({
+        title: "¿Estás Seguro?",
+        text: "No podrás revertir esto!",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonColor: "#3085d6",
+        cancelButtonColor: "#d33",
+        confirmButtonText: "Si, eliminar",
+        cancelButtonText: "Cancelar"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            Swal.fire({
+                title: "Eliminado!",
+                text: "El Post ha sido eliminado.",
+                icon: "success"
+            })
+
+            document.getElementById(`post${id}`).remove()
+        }
+    })
+}
+
